1a807e504b8b72fae3e58c4d9bfd5c35eac1b809
unknown <Tyler913@FLIB-11-1261.engg.ksu.edu>
2015-02-06 23:02:04 -0600

    Rendering chessboard

--- scripts/game.js
+++ scripts/game.js
@@ -1,25 +1,109 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var container, stats;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var camera, scene, renderer;
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var mouseX = 0, mouseY = 0;
 
-camera.position.z = 5;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+var origin = new THREE.Vector3();
 
-var render = function () {
-	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+init();
+animate();
 
-	renderer.render(scene, camera);
-};
 
-render();
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+	
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.x = 10;
+	camera.position.y = 12;
+	camera.lookAt(origin);
+
+	// scene
+	scene = new THREE.Scene();
+
+	var ambient = new THREE.AmbientLight( 0x444444 );
+	scene.add( ambient );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+	directionalLight.position.set( 0, 1, 1 ).normalize();
+	scene.add( directionalLight );
+
+	// load models
+	loadAssets();
+
+	window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function loadAssets() {
+	var assets = [
+		{OBJ: 'assets/chessboard/board.obj', MTL: 'assets/chessboard/board.mtl' }
+	];
+	
+	var onProgress = function ( xhr ) {
+		if ( xhr.lengthComputable ) {
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		}
+	};
+
+	var onError = function ( xhr ) {
+	};
+	
+	var onLoad = function(object) {
+		scene.add(object);
+	}
+
+	THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+
+	var loader = new THREE.OBJMTLLoader();
+	assets.forEach(function(asset) {
+		loader.load(asset.OBJ, asset.MTL, onLoad, onProgress, onError);
+	});
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+/*
+	mouseX = ( event.clientX - windowHalfX ) / 2;
+	mouseY = ( event.clientY - windowHalfY ) / 2;
+*/
+}
+
+//
+
+function animate() {
+	requestAnimationFrame( animate );
+	update();
+	render();
+}
+
+function update() {
+
+}
+
+function render() {
+	renderer.render( scene, camera );
+}
+
 
