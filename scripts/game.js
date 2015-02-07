fb2a57568d5899bd9f911b02f57a1219cd35d71f
unknown <guest-15@FLIB-13-1263.engg.ksu.edu>
2015-02-07 16:22:51 -0600

    Added box knight.

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,10 @@
 var container;
 
-var camera, scene, renderer, audio, board;
+var camera, scene, renderer, audio, board, player;
 
 var mouseX = 0, mouseY = 0;
+
+var loadProgress = {};
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -13,6 +15,103 @@ initGame();
 animate();
 
 function initGame() {
+	var loadAssets = function() {
+		var assets = [
+			{OBJ: 'assets/chessboard/board.obj', MTL: 'assets/chessboard/board.mtl', onLoad: function(object) {
+				scene.add(object);
+				object.position.y = -0.45;
+			}},
+			/*{OBJ: 'assets/test_cube/test_cube.obj', MTL: 'assets/test_cube/test_cube.mtl', onLoad: function(object) {
+				board.forEach(function(row) {
+					row.forEach(function(square) {
+						var clone = object.clone();
+						clone.position.copy(square.position);
+						scene.add(clone);
+					});
+				});
+			}},*/
+			{OBJ: 'assets/mini_box_knight/mini_knight.obj', MTL: 'assets/mini_box_knight/mini_knight.mtl', onLoad: function(object) {
+				object.rotation.y = 45;
+				player.object = object;
+				scene.add(object);
+			}}
+			/*{OBJ: 'assets/alphabet/A.obj', MTL: 'assets/alphabet/A.mtl', onLoad: function(object) {
+				var x = THREE.Math.randInt(0, 7);
+				var y = THREE.Math.randInt(0, 7);
+				object.rotateY(45);
+				object.position.copy(board[x][y].position);
+				scene.add(object);
+			}},*/
+		];
+
+		var onProgress = function ( xhr ) {
+			if ( xhr.lengthComputable ) {
+				var percentComplete = xhr.loaded / xhr.total * 100;
+
+				console.log( Math.round(percentComplete, 2) + '% downloaded' );
+			}
+		};
+
+		var onError = function ( xhr ) {
+			console.log('Error downloading ' + xhr.currentTarget.responseURL);
+		};
+
+		THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+
+		var loader = new THREE.OBJMTLLoader();
+		assets.forEach(function(asset) {
+			loader.load(asset.OBJ, asset.MTL, asset.onLoad, onProgress, onError);
+		});
+	}
+
+	var initGameBoard = function() {
+		var x = 3.5,
+			z = 3,
+			padding = 0.22,
+			width = 0.5;
+
+		board = [];
+		for(var i = 0; i < 8; i++) {
+			board[i] = [];
+			for(var j = 0; j < 8; j++) {
+				board[i][j] = {
+					position: new THREE.Vector3(
+						(x + padding) - (j * (2 * padding + width)),
+						0,
+						(z + padding) - (i * (2 * padding + width)))
+				}
+			}
+		}
+	}
+
+	function initScene() {
+		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+		camera.position.x = 10;
+		camera.position.y = 12;
+		camera.lookAt(origin);
+
+		// scene
+		scene = new THREE.Scene();
+
+		/*var ambient = new THREE.AmbientLight( 0x404040 );
+		scene.add(ambient);*/
+
+		var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+		directionalLight.position.set( 0, 1, 1 ).normalize();
+		scene.add(directionalLight);
+		
+		directionalLight = directionalLight.clone();
+		directionalLight.position.set( 0, -1, -1 ).normalize();	
+		scene.add(directionalLight);
+
+		directionalLight = directionalLight.clone();
+		directionalLight.position.set( 0, -1, 0).normalize();	
+		scene.add(directionalLight);
+
+		directionalLight = directionalLight.clone();
+		directionalLight.position.set( 0, 1, 0).normalize();	
+		scene.add(directionalLight);
+	}
 
 	container = $('#container');
 	
@@ -21,105 +120,21 @@ function initGame() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.append(renderer.domElement);
 
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.x = 10;
-	camera.position.y = 12;
-	camera.lookAt(origin);
+	window.addEventListener('resize', onWindowResize, false);
+	window.addEventListener('keyup', onKeyUp, false);
 
-	// scene
-	scene = new THREE.Scene();
-
-	var ambient = new THREE.AmbientLight( 0x444444 );
-	scene.add( ambient );
-
-	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-	directionalLight.position.set( 0, 1, 1 ).normalize();
-	scene.add( directionalLight );
-
-	// load models
 	initGameBoard();
+	resetPlayer();
+	initScene();
 	loadAssets();
-
-	window.addEventListener( 'resize', onWindowResize, false );
 }
 
-function initAudio() {
-	window.AudioContext = window.AudioContext || window.webkitAusioContext;
-	audio = new AudioContext();
-
-	var audioSources = [
-		"assets/audio/bg.mp3"
-	];
-
-	var onLoad = function(response){
-		audio.decodeAudioData(request.response, onDecode, onError);
-	};
-
-	var onDecode = function(buffer) {
-		//Handle decoded audio.
-	}
-
-	var onError = function(error) {
-
-	}
-
-	audioSources.forEach(function(source) {
-		$.get(source, onLoad)
-	})
-}
-
-function loadAssets() {
-	var assets = [
-		{OBJ: 'assets/chessboard/board.obj', MTL: 'assets/chessboard/board.mtl', onLoad: function(object) {
-			scene.add(object);
-			object.position.y = -0.45;
-		}},
-		{OBJ: 'assets/test_cube/test_cube.obj', MTL: 'assets/test_cube/test_cube.mtl', onLoad: function(object) {
-			board.forEach(function(row) {
-				row.forEach(function(square) {
-					var clone = object.clone();
-					clone.position.copy(square.position);
-					scene.add(clone);
-				});
-			});
-		}}
-	];
-	
-	var onProgress = function ( xhr ) {
-		if ( xhr.lengthComputable ) {
-			var percentComplete = xhr.loaded / xhr.total * 100;
-			console.log( Math.round(percentComplete, 2) + '% downloaded' );
-		}
-	};
-
-	var onError = function ( xhr ) {
-	};
-
-	THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-
-	var loader = new THREE.OBJMTLLoader();
-	assets.forEach(function(asset) {
-		loader.load(asset.OBJ, asset.MTL, asset.onLoad, onProgress, onError);
-	});
-}
-
-function initGameBoard() {
-	var x = -3.5,
-		z = -3,
-		padding = 0.22,
-		width = 0.5;
-
-	board = [];
-	for(var i = 0; i < 8; i++) {
-		board[i] = [];
-		for(var j = 0; j < 8; j++) {
-			board[i][j] = {
-				position: new THREE.Vector3(
-					(z + padding) + (i * (2 * padding + width)),
-					0,
-					(x + padding) + (j * (2 * padding + width)))
-			}
-		}
+function resetPlayer() {
+	player = {
+		x:0,
+		y:0,
+		object: (!!player) ? player.object : null,
+		moveCount: 0
 	}
 }
 
@@ -135,11 +150,25 @@ function onWindowResize() {
 
 }
 
-function onDocumentMouseMove( event ) {
-/*
-	mouseX = ( event.clientX - windowHalfX ) / 2;
-	mouseY = ( event.clientY - windowHalfY ) / 2;
-*/
+function onKeyUp(event) {
+	if(event.keyCode < 37 || event.keyCode > 40) {
+		return;
+	}
+
+	var prevX = player.x;
+	var prevY = player.y;
+	switch(event.keyCode) {
+		case 37: player.x -= 1; break; //Left
+		case 38: player.y += 1; break; //Up
+		case 39: player.x += 1; break; //Right
+		case 40: player.y -= 1; break; //Down
+	}
+	player.x = THREE.Math.clamp(player.x, 0, 7);
+	player.y = THREE.Math.clamp(player.y, 0, 7);
+
+	if(prevY != player.y || prevX != player.x) {
+		player.moveCount++;	
+	}
 }
 
 //
@@ -151,11 +180,18 @@ function animate(time) {
 }
 
 function update() {
+	if(!!player.object) {
+		player.object.position.copy(board[player.x][player.y].position);
+	}
 
+	
 }
 
 function render() {
 	renderer.render(scene, camera);
+
+	var $moveCount = $('.moveCount');
+	$moveCount.text(player.moveCount);
 }
 
 
